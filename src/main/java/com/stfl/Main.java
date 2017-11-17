4c22a28379e89867e06433afcbe7d4e4b48adc48
hujie <hujie187@126.com>
2017-11-17 15:59:12 +0800

    remove test message

--- src/main/java/com/stfl/Main.java
+++ src/main/java/com/stfl/Main.java
@@ -18,8 +18,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("test");
-
         if (args.length != 0) {
             startCommandLine(args);
         }
